d7e4f06821922a5636fb7f460d1ac15010456090
MANU KUMAR H N <manukumarhn86@gmail.com>
2026-01-19 14:07:40 +0530

    Day 5 : Contains Duplicate using HashSet (java)

---
+++ Hashing/ContainsDuplicate.java
@@ -0,0 +1,10 @@
+import java.util.HashSet;
+public class  ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums){
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(!set.add(num))return true;
+        }
+        return false;
+    }
+}
