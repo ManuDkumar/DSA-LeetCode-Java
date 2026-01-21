0b29b1e54bb8d6e92010b29dd76f03a565585e27
MANU KUMAR H N <manukumarhn86@gmail.com>
2026-01-21 15:15:14 +0530

    Day 7: Valid parentheses (java)

---
+++ Stack/ValidParentheses.java
@@ -0,0 +1,27 @@
+public class  ValidParentheses {
+    public boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0 ; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '[' || ch == '{' || ch == '('){
+                stack.add(ch);
+            }
+            else {
+                if (stack.isEmpty()) return false;
+                if(ch == ')' && stack.peek() == '('){
+                    stack.pop();
+                }
+                else if(ch == ']' && stack.peek() == '['){
+                    stack.pop();
+                }
+                else if(ch == '}' && stack.peek() == '{'){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty(); 
+    }
+}
