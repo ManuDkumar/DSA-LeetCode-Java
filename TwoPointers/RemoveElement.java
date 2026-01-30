720c89d5b9966c2891149eec8a46ef691c1d1efe
MANU KUMAR H N <manukumarhn86@gmail.com>
2026-01-30 15:16:35 +0530

    Day 16 : Remove Element (java)

---
+++ TwoPointers/RemoveElement.java
@@ -0,0 +1,10 @@
+public class RemoveElement {
+    public int removeElement(int[] nums,int val){
+        int k = 0;
+        for (int i : nums) {
+            if (i != val)
+                nums[k++] = i;
+        } 
+        return k; 
+    }
+}
