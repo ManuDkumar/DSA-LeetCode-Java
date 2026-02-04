287e6895763e62f3e91e14affd4c0f1126a8b208
MANU KUMAR H N <manukumarhn86@gmail.com>
2026-02-04 21:31:12 +0530

    Implementing solution for problem Maximum Sub Array Sum using kadane algorithm

---
+++ Arrays/MaximumSumCircularSubarray.java
@@ -0,0 +1,20 @@
+public class MaximumSumCircularSubarray {
+    public int maximumSumCircular(int[] nums){
+        int total = 0;
+        int maxSum = nums[0];
+        int curMax = 0;
+        int minSum = nums[0];
+        int curMin = 0;
+
+        for (int num : nums) {
+            curMax = Math.max(curMax + num, num);
+            maxSum = Math.max(maxSum, curMax);
+
+            curMin = Math.min(curMin + num, num);
+            minSum = Math.min(minSum, curMin);
+
+            total += num;
+        }
+        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
+    }
+}
