d40334fbb1de679248dc2bd8eb5ba5e99fa54ca2
MANU KUMAR H N <manukumarhn86@gmail.com>
2026-02-05 19:05:33 +0530

    Implementing solution for Leet Code problem Non Overlapping Intervals using Greedy Method of the pattern Interval Scheduling (Sort by End)

--- Greedy/NonOverlappingIntervals.java
+++ Greedy/NonOverlappingIntervals.java
@@ -4,13 +4,12 @@ class NonOverlappingIntervals {
         if (intervals.length <= 1)
             return 0;
 
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[1], b[1])); //Sort by End
         int count = 0;
         int end = intervals[0][1];
          for(int i = 1; i < intervals.length; i++) {
            if (intervals[i][0] < end) {
                 count++;
-                end = Math.min(end, intervals[i][1]);
            } 
            else {
                 end = intervals[i][1];
