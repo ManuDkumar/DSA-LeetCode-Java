601dbcda2e3b38c55f04162a62d3e304e3236234
MANU KUMAR H N <manukumarhn86@gmail.com>
2026-02-08 21:26:43 +0530

    feat: Binary Tree Level Order Traversal (Leet Code 102) using BFS level-order traversal. Time Complexity : O(n), Space Complexity : O(n)

---
+++ Trees/LevelOrderTraversal.java
@@ -0,0 +1,26 @@
+public class LevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root){
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                currentLevel.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(currentLevel);
+        }
+        return result;
+    }
+}
