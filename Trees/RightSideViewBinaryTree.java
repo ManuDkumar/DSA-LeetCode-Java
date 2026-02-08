2b5a088dff203485736e56614afb068cd6101aa5
MANU KUMAR H N <manukumarhn86@gmail.com>
2026-02-08 21:31:53 +0530

    feat: Binary Tree Right Side View (Leet Code 199) using using BFS Level-Order Traversal. Time Complexity O(n), Space Complexity O(D) - Where D is tree diameter

---
+++ Trees/RightSideViewBinaryTree.java
@@ -0,0 +1,26 @@
+public class RightSideViewBinaryTree {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (i == size - 1) {
+                    result.add(node.val);
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return result;
+    }
+}
