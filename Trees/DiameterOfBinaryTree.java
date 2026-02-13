f6fc6ee099e96a37649798f59499c9be5ee018cc
MANU KUMAR H N <manukumarhn86@gmail.com>
2026-02-13 22:26:25 +0530

    feat : Diameter Of Binary Tree (Leet Code 543) using recursion. Time complexity O(n), Space Complexity O(h).

---
+++ Trees/DiameterOfBinaryTree.java
@@ -0,0 +1,16 @@
+public class DiameterOfBinaryTree {
+    int diameter = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return diameter;
+    }
+    public int depth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftDepth = depth(node.left);
+        int rightDepth = depth(node.right);
+        diameter = Math.max(diameter, leftDepth + rightDepth);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
