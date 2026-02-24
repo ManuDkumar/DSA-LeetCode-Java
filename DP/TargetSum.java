16f3f1ff14074505ea20b79b33c831b4fbf39a9b
MANU KUMAR H N <manukumarhn86@gmail.com>
2026-02-24 20:06:25 +0530

    feat : Target Sum (Leet Code 494) using 1D DP with space optimization. Time Complexity O(N*subsetTarget), space complexity O(subsetTarget)

---
+++ DP/TargetSum.java
@@ -0,0 +1,20 @@
+class TargetSun {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if ((sum + target)%2 != 0 || Math.abs(target) > sum) {
+            return 0;
+        }
+        int newTarget = (sum + target)/2;
+        int[] dp = new int[newTarget + 1];
+        dp[0] = 1;
+        for (int num : nums) {
+            for(int curTarget = newTarget; curTarget >= num; curTarget-- ) {
+                dp[curTarget] = dp[curTarget] + dp[curTarget - num];
+            }
+        }
+        return dp[newTarget];
+    }
+}
