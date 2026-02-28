de151003565fdf2237ab779e23716b3bef88f26c
MANU KUMAR H N <manukumarhn86@gmail.com>
2026-02-28 17:13:24 +0530

    feat : Maximal Square (Leet Code 221) using 2D DP. O(M*N) Time and Space Complexity.

---
+++ DP/MaximalSquare.java
@@ -0,0 +1,24 @@
+public class MaximalSquare {
+   private int maxSideLen = 0;
+    public int maximalSquare(char[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == '1') {
+                    if(i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
+                    }
+                    maxSideLen = Math.max(maxSideLen, dp[i][j]);
+                }
+            }
+        }        
+        return maxSideLen * maxSideLen;
+    }
+}
