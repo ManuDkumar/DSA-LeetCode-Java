ae7ae1a0bd0f854fd553d866077b2d5293565709
MANU KUMAR H N <manukumarhn86@gmail.com>
2026-03-16 16:13:14 +0530

    feat : Reverse Linked List (Leet code 206) using Recursion. time and space O(n).

---
+++ LinkedList/ReverseLinkedList.java
@@ -0,0 +1,11 @@
+class ReverseLinkedList {
+   public ListNode reverseList(ListNode head) {        
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode newNode = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newNode;
+    }
+}
