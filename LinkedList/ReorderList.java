24821cbd82b2420f271d1819c210c924ddc8ac96
MANU KUMAR H N <manukumarhn86@gmail.com>
2026-03-19 22:25:32 +0530

    feat : Reorder List (Leet code 143) using linked list finding middle, reversing and merging logics.

---
+++ LinkedList/ReorderList.java
@@ -0,0 +1,45 @@
+public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+public class ReorderList {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null)
+            return;
+
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode second = slow.next;
+        slow.next = null;
+        second = reverseList(second);
+
+        ListNode first = head;
+        while (second != null) {
+            ListNode t1 = first.next;
+            ListNode t2 = second.next;
+
+            first.next = second;
+            second.next = t1;
+
+            first = t1;
+            second = t2;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newNode = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newNode;
+    }
+}
