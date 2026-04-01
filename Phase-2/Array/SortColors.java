ff8f83110fd092d2299b623ff73ad936fc7d5abc
MANU KUMAR H N <manukumarhn86@gmail.com>
2026-04-01 22:05:17 +0530

    feat : Sort Colors (Leet code 75) using Dutch Flag algorithm. with time O(n), and space O(1).

---
+++ Phase-2/Array/SortColors.java
@@ -0,0 +1,26 @@
+class SortColors {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while(mid <= high) {
+            if(nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if(nums[mid] == 2) {
+                swap(nums, mid, high);
+                high--;
+            } else {
+                mid++;
+            }
+        }
+    }
+
+    public void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+}
